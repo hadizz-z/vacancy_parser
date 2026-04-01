0a1a9fbd45a56939478eea6a14eddec81d586478
sentiurinaDasha <sentdasha106@gmail.com>
2026-04-01 12:02:48 +0300

    Update job_market_analytics.rb
    fix

--- lib/job_market_analytics.rb
+++ lib/job_market_analytics.rb
@@ -5,9 +5,10 @@ require_relative "job_market_analytics/reporters/html_reporter"
 module JobMarketAnalytics
   class Error < StandardError; end
 
-  def self.generate_report(vacancies_data, title = "Job Market Report", total = "0", output_path = nil)
+  def self.generate_report(vacancies_data, title = "Job Market Report", total = "0", error = nil, output_path = nil)
     vacancies = vacancies_data.map { |data| Models::Vacancy.new(data) }
     reporter = Reporters::HtmlReporter.new(vacancies, title, total, output_path)
+    reporter.error = error if error
     reporter.generate_and_open
   end
   
