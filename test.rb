ed548d9d4c880ec7b655a019ca450ec1ec617f9e
sentiurinaDasha <sentdasha106@gmail.com>
2026-04-01 12:04:44 +0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,13 +1,20 @@
 require_relative 'lib/job_market_analytics'
 require_relative 'lib/job_market_analytics/api/head_hunter_api'
 
-puts "input keywords"
+puts "Введите ключевые слова для поиска (например, ruby developer):"
 keywords = gets.chomp
 
 hh_api = JobMarketAnalytics::Api::HeadHunterApi.new
-
 vacancies_data = hh_api.vacancy_request(keywords)
 
-puts "Generating report..."
-result = JobMarketAnalytics.generate_report(vacancies_data, "Vacancy Market Report", hh_api.total_salary)
-puts "Done! File saved to: #{result}"
+if hh_api.success?
+  puts "Найдено вакансий: #{vacancies_data.size}"
+  puts "Генерируем отчет..."
+  JobMarketAnalytics.generate_report(vacancies_data, "Отчет по вакансиям: #{keywords}", hh_api.total_salary)
+else
+  puts "Ошибка: #{hh_api.error}"
+  puts "Генерируем отчет с сообщением об ошибке..."
+  JobMarketAnalytics.generate_report([], "Отчет по вакансиям: #{keywords}", 0, hh_api.error)
+end
+
+puts "Готово!"
